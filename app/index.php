cf7150ac1cf58a9f726f320bf0ce4fe04dc1328f
unknown <marselina88@list.ru>
2021-09-02 19:34:07 +0300

    2.09
    постановка на галп и с опенсервер

---
+++ app/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="css/app.min.css">
+	<title>Bar</title>
+</head>
+<body>
+<h1>HELLO</h1>
+<?php echo "Предисловие";?>
+<img src="images/dest/eda2.jpg">
+<script src="js/app.min.js"></script>
+</body>
+</html>
