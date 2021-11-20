f747882e8497b165d286ec4421ee9757115e107b
unknown <marselina88@list.ru>
2021-11-20 20:15:37 +0300

    модальное окно срабатывает
    Добавила в форму входа action='/user/login/' и изменила ajax

--- app/views/layouts/header.php
+++ app/views/layouts/header.php
@@ -70,7 +70,37 @@
                 <div class="backgroud2 d-flex justify-content-end close2"  data-dismiss="modal"><img src="/images/dest/g8lack.png"  alt=""/></div>                
             
             <div class="modal-body">
-                
+              <div class="container-fluid col-lg-12 d-flex flex-column">
+
+     <div class="col-sm-4 col-sm-offset-4 padding-right">
+
+                <?php if (isset($errors) && is_array($errors)): ?>
+                    <ul>
+                        <?php foreach ($errors as $error): ?>
+                            <li> - <?php echo $error; ?></li>
+                        <?php endforeach; ?>
+                    </ul>
+                <?php endif; ?>
+
+                <div class="signup-form"><!--sign up form-->
+                    <h2>Вход на сайт</h2>
+                    <form action="/user/login/" method="post">
+                        <input type="email" name="email" placeholder="E-mail" value="<?php echo $email; ?>"/>
+                        <input type="password" name="password" placeholder="Пароль" value="<?php echo $password; ?>"/>
+                        <input type="submit" name="submit" class="btn btn-default" value="Вход" />
+                    </form>
+                </div><!--/sign up form-->
+
+
+                <br/>
+                <br/>
+            </div>
+        
+            
+            <div class="block-registration col-lg-12 d-flex justify-content-end backgroud2">
+                <a href="/user/register/">регистрация</a>
+            </div>
+</div>  
             </div>
             
         </div>
