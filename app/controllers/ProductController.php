0e74788177d4c376956ebf23af9465f62dd2efce
unknown <marselina88@list.ru>
2021-12-12 16:45:37 +0300

    Update ProductController.php

--- app/controllers/ProductController.php
+++ app/controllers/ProductController.php
@@ -6,8 +6,7 @@ class ProductController
 	
 	public function actionView($productId)
     {
-        $categories = array();
-        $categories = Category::getCategoriesList();
+        
         $product = Product::getProductById($productId);
 
         require_once(ROOT . '/views/product/view.php');
